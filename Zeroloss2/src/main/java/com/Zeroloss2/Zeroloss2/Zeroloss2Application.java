b2b88cc994180f75d9aa48483bc7b30275880efe
cross-wata <8watanabe11@gmail.com>
2026-02-24 11:45:56 +0900

    2/24作成

---
+++ Zeroloss2/src/main/java/com/Zeroloss2/Zeroloss2/Zeroloss2Application.java
@@ -0,0 +1,13 @@
+package com.Zeroloss2.Zeroloss2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zeroloss2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zeroloss2Application.class, args);
+	}
+
+}

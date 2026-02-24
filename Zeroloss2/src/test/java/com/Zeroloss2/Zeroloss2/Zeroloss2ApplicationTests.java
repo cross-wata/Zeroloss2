b81390f82490496227a77a1b7b51fd67a8b45539
cross-wata <8watanabe11@gmail.com>
2026-02-24 11:45:56 +0900

    2/24作成

---
+++ Zeroloss2/src/test/java/com/Zeroloss2/Zeroloss2/Zeroloss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zeroloss2.Zeroloss2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zeroloss2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ddaa1161c951312de58cebb06685af4663c01e42
suzumerion <sparrow2025.7.3.6@gmail.com>
2026-03-12 13:29:48 +0900

    ボタンを押してランダムメッセージを表示させてDBにメッセージを保存するため、 day と message をまとめて受け取る ので、専用クラスを作ります。

---
+++ Zeroloss2/src/main/java/com/Zeroloss2/Zeroloss2/dto/PressRequest.java
@@ -0,0 +1,23 @@
+package com.Zeroloss2.Zeroloss2.dto;
+
+public class PressRequest {
+
+    private int day;
+    private String message;
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
